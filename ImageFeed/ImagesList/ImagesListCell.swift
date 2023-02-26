a013bf190323eb40eb2e59984ad085469cbb650d
Dmitry <d1mon227@yandex.ru>
2023-02-26 19:37:57 +0200

    Выполнен спринт 8.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,43 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Dmitry Medvedev on 21.02.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var gradientImageView: UIImageView!
+    
+    static let reuseIdentifier = "ImagesListCell"
+
+    override func layoutSubviews() {
+        gradientImageView.layer.sublayers = nil
+        setupGradient()
+    }
+    
+    override func prepareForReuse() {
+        gradientImageView.layer.sublayers = nil
+    }
+    
+    func setupGradient() {
+        let height = gradientImageView.bounds.height
+        let width = gradientImageView.bounds.width
+        
+        let colorTop = UIColor.ypBlack.withAlphaComponent(0.0).cgColor
+        let colorBottom = UIColor.ypBlack.withAlphaComponent(0.2).cgColor
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        gradient.colors = [colorTop, colorBottom]
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientImageView.layer.addSublayer(gradient)
+    }
+}
